d30514d9c3e3081e2043a1ea2d1a3a3b041654bc
anik00 <anik.chakraborty@orbitax.com>
2024-10-31 18:03:06 +0600

    Added Student Controller

---
+++ DAL/SchoolContext.cs
@@ -0,0 +1,18 @@
+﻿using RepositoryPattern.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace RepositoryPattern.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
